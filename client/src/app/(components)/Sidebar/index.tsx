6256228e23da62bd17c836729bd89413a285e0e3
Adithyan T <>
2024-11-11 01:38:01 +0530

    redux toolkit setup complete

---
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { LockIcon } from "lucide-react";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const Sidebar = () => {
+  const [showProjects, setShowProjects] = useState(false);
+  const [showPriority, setShowPriority] = useState(false);
+
+  const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300
+  h-full z-40 dark:bg-black overflow-y-auto bg-white 
+  w-64
+  `;
+
+  return (
+    <div className={sidebarClassNames}>
+      <div className="flex h-[100%] w-full flex-col justify-start">
+        {/* TOP LOGO */}
+        <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
+          <div className="text-xl font-bold text-gray-900 dark:text-white">
+            PROMAN
+          </div>
+        </div>
+        {/* TEAM */}
+        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={30}
+            unoptimized
+            height={30}
+          />
+          <div className="">
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              PRO TEAM
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] size-3 text-gray-500 dark:text-gray-400" />
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
+          </div>
+        </div>
+        {/* NAVBAR LINKS */}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
